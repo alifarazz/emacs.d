dddfeb82d1782ae6aa49f5579d2b8a6f8b863200
luce <alifarazdaghi@gmail.com>
2019-03-22 04:22:37 +0430

    Add Clojure support

--- init.el
+++ init.el
@@ -707,9 +707,39 @@
 
 ;; support for the rest of cpp files
 (dolist (mode (list ;'("\\.h\\'" . c++-mode)
-               '("\\.hpp\\'" . c++-mode)
-               '("\\.cc\\'" . c++-mode)))
-  (add-to-list 'auto-mode-alist mode))
+                    '("\\.hpp\\'" . c++-mode)
+                    '("\\.cc\\'" . c++-mode)))
+         (add-to-list 'auto-mode-alist mode))
+;; Clojure
+(use-package clojure-mode
+  :ensure t
+  :mode ("\\.clj\\'" . clojure-mode))
+
+(use-package cider
+  :ensure t
+  :delight
+  :config  (add-hook 'clojure-mode-hook #'cider-mode))
+
+(use-package parinfer
+  :ensure t
+  :bind
+  (("C-," . parinfer-toggle-mode))
+  :init
+  (progn
+    (setq parinfer-extensions
+          '(defaults       ; should be included.
+             pretty-parens  ; different paren styles for different modes.
+             ;; evil           ; If you use Evil.
+             ;; lispy          ; If you use Lispy. With this extension, you should install Lispy and do not enable lispy-mode directly.
+             ;; paredit        ; Introduce some paredit commands.
+             ;; smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
+             ;; smart-yank   ; Yank behavior depend on mode.
+             )) 
+    (add-hook 'clojure-mode-hook #'parinfer-mode)
+    (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
+    (add-hook 'common-lisp-mode-hook #'parinfer-mode)
+    (add-hook 'scheme-mode-hook #'parinfer-mode)
+    (add-hook 'lisp-mode-hook #'parinfer-mode)))
 
 (provide 'init)
 
